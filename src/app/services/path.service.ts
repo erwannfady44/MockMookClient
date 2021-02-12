646a440b5a4da614f20f9cee9baa408e96924414
Antoine Petit <antoine.petit@etu.univ-nantes.fr>
2021-02-12 14:25:25 +0100

    Module hmtl + html modifier/ajout module a un parcour

--- src/app/services/path.service.ts
+++ src/app/services/path.service.ts
@@ -35,7 +35,7 @@ export class PathService {
             res => {
                 this.allPath = [];
                 res.json.forEach(path => {
-                    this.allPath.push(new PathModel(path.title, path.description, path.pseudo, path.idPath, null));
+                    this.allPath.push(new PathModel(path.title, path.description, path.pseudo, path.idPath, path.date));
                 });
                 this.emitAllPathSubject();
             }, error => {

fdb96c93aacc3b90a6de8bac794b76daaf3e7428
Antoine Petit <antoine.petit@etu.univ-nantes.fr>
2021-02-23 15:03:26 +0100

    route View all module + view ressource

---
+++ src/app/view-all-module/view-all-module.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewAllModuleComponent } from './view-all-module.component';
+
+describe('ViewAllModuleComponent', () => {
+  let component: ViewAllModuleComponent;
+  let fixture: ComponentFixture<ViewAllModuleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ViewAllModuleComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ViewAllModuleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

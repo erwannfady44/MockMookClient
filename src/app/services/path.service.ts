0ffbee6ba0ced5c2998e2dc46921860792ef06c1
E194482T <EF2646cookie>
2021-02-24 14:15:17 +0100

    correction bug

--- src/app/services/path.service.ts
+++ src/app/services/path.service.ts
@@ -32,18 +32,20 @@ export class PathService {
         return new Promise(resolve => this.http.get<any>(`${this.URL}/path/`).subscribe(
             res => {
                 this.allPath = [];
-                res.json.forEach(path => {
-                    this.allPath.push(new PathModel(path.title, path.description, path.pseudo, path.idPath, new Date(path.date)));
-                });
-                this.allPath.sort((a, b) => {
-                    if (a._date > b._date) {
-                        return -1;
-                    } else if (a._date < b._date) {
-                        return 1;
-                    } else {
-                        return 0;
-                    }
-                });
+                if (res.json) {
+                    res.json.forEach(path => {
+                        this.allPath.push(new PathModel(path.title, path.description, path.pseudo, path.idPath, new Date(path.date)));
+                    });
+                    this.allPath.sort((a, b) => {
+                        if (a._date > b._date) {
+                            return -1;
+                        } else if (a._date < b._date) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    });
+                }
                 resolve(this.allPath);
             }, error => {
                 console.log(error.message);
@@ -55,11 +57,13 @@ export class PathService {
         return new Promise(resolve => this.http.get<any>(`${this.URL}/path/${idPath}`).subscribe(
             res => {
                 this.path = new PathModel(res.title, res.description, res.pseudo, res.idPath, new Date(res.date));
-                res.modules.forEach((module) => {
-                    const m = new ModuleModel(res.idPath, module.pseudo, module.title, module.description);
-                    m._idModule = module.idModule;
-                    this.path.addModule(m);
-                });
+                if (res.modules) {
+                    res.modules.forEach((module) => {
+                        const m = new ModuleModel(res.idPath, module.pseudo, module.title, module.description);
+                        m._idModule = module.idModule;
+                        this.path.addModule(m);
+                    });
+                }
                 resolve(this.path);
             }, error => {
                 console.log(error.message);

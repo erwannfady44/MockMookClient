13c064fba61b62b1572dcf5018352092cfd1152d
erwannfady44 <erwann.fady@gmail.com>
2021-04-08 15:05:58 +0200

    correction bug affichage lecteur youtube

--- src/app/resource-video/resource-video.component.ts
+++ src/app/resource-video/resource-video.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ResourceModel} from '../model/Resource.model';
 import {AppService} from '../services/app.service';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
     selector: 'app-resource-video',
@@ -10,14 +11,18 @@ import {AppService} from '../services/app.service';
 export class ResourceVideoComponent implements OnInit {
     @Input() resource: ResourceModel;
 
-    constructor(private app: AppService) {
+    constructor(private app: AppService, private sanitizer: DomSanitizer) {
     }
 
     ngOnInit(): void {
-        console.log(this.resource._url.substring(17));
+        console.log(this.url());
     }
 
     getTime(): string {
         return this.app.dateDiff(this.resource._date, Date.now());
+    }
+
+    url(): SafeResourceUrl {
+        return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + this.resource._url.substring(17));
     }
 }
